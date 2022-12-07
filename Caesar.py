e1c8256140c0cc85aaceee6e893781485e9fc9ce
sleklere <santiagoleklere@gmail.com>
2022-12-07 16:35:05 -0300

    code refactored, comments added, some functionalities improved/fixed

--- Caesar.py
+++ Caesar.py
@@ -1,55 +1,63 @@
-alphabet_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-                 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
-                 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-                 'Y', 'Z']
+import string
 
+# Creating a list of all alphanumeric characters
+alphabet_chars = []
+for num in string.digits:
+    alphabet_chars.extend(num)
+for char in string.ascii_letters:
+    alphabet_chars.extend(char)
 
-def split_characters(text):
-    split_text = text.split()
-    characters = []
-    for word in split_text:
-        characters += [c for c in word]
-    return characters
+#################################
 
+# Input to choose encryption or decryption
+while True:
+    print('------------------------------')
+    encrypt_or_decrypt = input('Enter 1(Encrypt) or 2(Decrypt):\n')
+    if encrypt_or_decrypt == '1' or encrypt_or_decrypt == '2':
+        break
+    else:
+        print('#######################################################')
+        print('-- Please enter either 1 to encrypt, or 2 to decrypt --')
+        print('#######################################################')
+        pass
 
+# Input for key number
 while True:
     try:
-        print('--------------------------------')
-        encrypt_or_decrypt = input('Enter 1(Encrypt) or 2(Decrypt):\n')
-        if encrypt_or_decrypt == '1' or encrypt_or_decrypt == '2':
-            break
-        else:
-            pass
-    except ValueError:
-        continue
-while True:
-    try:
-        print('-------------------')
+        print('-----------------')
         key_number = int(input('Enter Key Number:\n'))
         break
     except ValueError:
+        print('#################################')
+        print('-- Please enter a valid number --')
+        print('#################################')
         continue
-print('--------------------------')
-text = input('Text to Encrypt/Decrypt:\n')
-encrypted_text = ''
-decrypted_text = ''
-text_characters = split_characters(text)
-for character in text_characters:
-    if character in alphabet_list:
-        ch_index = alphabet_list.index(character)
-        if '1' in encrypt_or_decrypt:
-            new_ch_index = ch_index + key_number
-            new_character = alphabet_list[new_ch_index]
-            encrypted_text += new_character
-        else:
-            new_ch_index = ch_index - key_number
-            new_character = alphabet_list[new_ch_index]
-            decrypted_text += new_character
 
-print("--------------------")
-if '1' in encrypt_or_decrypt:
-    print("Your encrypted text:")
-    print(encrypted_text)
-else:
-    print("Your decrypted text:")
-    print(decrypted_text)
+# Input for text
+while True:
+    try:
+      print('-------------------------------------------')
+      text = input('Enter the text to Encrypt/Decrypt:\n')
+      for char in text:
+        if char not in alphabet_chars:
+          raise ValueError
+      break
+    except ValueError:
+      print('******************************************************************')
+      print('-- Please enter text that only contains alphanumeric characters --')
+      print('******************************************************************')
+      continue
+
+output_text = ''
+
+
+# the '% len (alphabet_chars)' makes sure that if the index goes beyond the list's length, it 'starts over' at index 0, as if it were a circle
+# anything below the result of len(alphabet_chars) will return whatever number resulted from alphabet_chars[(alphabet_chars.index(c) + shift). but if, for example, the length is 26 and the other number is 27, the modulus is equal to 1, which is the index position that corresponds.
+# Use a ternary operator to determine the shift direction (encrypt or decrypt)
+shift = key_number if encrypt_or_decrypt == 1 else - key_number
+encrypted_chars = [alphabet_chars[(alphabet_chars.index(c) + shift) % len(alphabet_chars)] for c in text]
+output_text = "".join(encrypted_chars)
+
+print("-----------------")
+print("Your result text:")
+print(output_text)
